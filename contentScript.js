3e6d2110c6c5575d6475dadf969c32d8345ef096
Markus Koeppen <mkoeppen@posteo.de>
2018-11-22 22:04:45 +0100

    init chrome extension

---
+++ contentScript.js
@@ -0,0 +1,98 @@
+/**
+ * BASE TEST
+ */
+
+function TestBase(name) {
+    this.name = name;
+}
+
+/**
+ * Tests
+ */
+
+function HtmlImgTest(name) {
+    this.result = undefined;
+    this.errorList = [];
+}
+HtmlImgTest.prototype.runTest = function() {
+    var testModule = this,
+        imgElements = document.querySelectorAll("img");
+
+    imgElements.forEach(function(element){ 
+        testModule.testHasSrcAttribute(element);
+        testModule.testHasAltAttribute(element);
+    });
+
+    return this.errorList;
+}
+HtmlImgTest.prototype.testHasSrcAttribute = function(element) {
+    var elementAttr = element.getAttribute("src");
+    if(elementAttr === null || typeof elementAttr === "undefined") {
+        this.errorList.push({
+            test: "testHasSrcAttribute",
+            element: element,
+            msg: "Ein img-Tag benötigt immer ein src-Attribute"
+        });
+    }
+}
+HtmlImgTest.prototype.testHasAltAttribute = function(element) {
+    var elementAttr = element.getAttribute("alt");
+    if(elementAttr === null || typeof elementAttr === "undefined") {
+        this.errorList.push({
+            test: "testHasAltAttribute",
+            element: element,
+            msg: "Ein img-Tag benötigt immer ein alt-Attribute"
+        });
+    }
+}
+
+function HtmlImgA(name) { 
+    this.result = undefined;
+    this.errorList = [];
+}  
+HtmlImgA.prototype.runTest = function() {
+
+    return this.errorList;
+}
+
+
+/**
+ * Tester
+ */
+
+function Tester() {
+    this.tests = [
+        new HtmlImgTest("Test"),
+        new HtmlImgA("A"),
+    ];
+}
+Tester.prototype.init = function() {
+    console.log("init Tester");
+}
+Tester.prototype.runTests = function() {
+    console.log("run Tester");
+    var allErros = [];
+    for (var i=0; i<this.tests.length; i++) {
+        var currentTest = this.tests[i];
+        if(typeof currentTest.runTest === "function") {
+            allErros = allErros.concat(currentTest.runTest());
+        }
+    }
+    console.table(allErros)
+}
+
+/**
+ * Init Injection
+ */
+
+function init() {
+    var currentTester = new Tester();
+    currentTester.init();
+    currentTester.runTests();
+}
+  
+// Include guard: only execute once
+// if (!window.injected) {
+    injected = true;
+    init();
+// }
